e6138988b64eeebb1aa3a2e39bc32a7b1071fac4
David Danford <ddanford@gmail.com>
2012-01-17 09:43:25 -0700

    Finished example 1 Extra Credit.

--- project02/ex1.rb
+++ project02/ex1.rb
@@ -1,7 +1,9 @@
-puts "Hello World!"
-puts "Hello Again"
-puts "I like typing this."
-puts "This is fun."
-puts 'Yay! Printing.'
-puts "I'd much rather you 'not'."
-puts 'I "said" do not touch this.'
+#puts "Hello World!"
+#puts "Hello Again"
+#puts "I like typing this."
+#puts "This is fun."
+#puts 'Yay! Printing.'
+#puts "I'd much rather you 'not'."
+#puts 'I "said" do not touch this.'
+#This is a comment
+puts 'Printing another line, and only one line'
