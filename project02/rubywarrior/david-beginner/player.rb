c75ff76bde35f2e99342467e0fb5b125f6093d1c
David Danford <ddanford@gmail.com>
2012-01-17 10:19:10 -0700

    Passed level 3.

--- project02/rubywarrior/david-beginner/player.rb
+++ project02/rubywarrior/david-beginner/player.rb
@@ -1,10 +1,18 @@
 class Player
   def play_turn(warrior)
     # add your code here
-	if warrior.feel.enemy?
-		warrior.attack!
+	if warrior.health < 10
+		if warrior.feel.enemy?
+			warrior.walk!(:backward)
+		else
+			warrior.rest!
+		end
 	else
-		warrior.walk!
+		if warrior.feel.enemy?
+			warrior.attack!
+		else
+			warrior.walk!
+		end
 	end
   end
 end
