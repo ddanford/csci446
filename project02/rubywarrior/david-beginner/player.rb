2b7eef1333455e0e07ca642cccb6cddd354e9a52
David Danford <ddanford@gmail.com>
2012-01-18 13:53:14 -0700

    Completed level 4.

--- project02/rubywarrior/david-beginner/player.rb
+++ project02/rubywarrior/david-beginner/player.rb
@@ -1,18 +1,51 @@
 class Player
   def play_turn(warrior)
     # add your code here
-	if warrior.health < 10
-		if warrior.feel.enemy?
-			warrior.walk!(:backward)
-		else
-			warrior.rest!
-		end
-	else
+	if warrior.health >= 20
 		if warrior.feel.enemy?
 			warrior.attack!
+			@previousaction = :attack
 		else
 			warrior.walk!
+			@previousaction = :advance
+		end
+	else
+		if @previoushealth > warrior.health and (@previousaction == :advance or @previousaction == :retreat)
+			@ranged = true
+			puts 'Ranged!'
+		end
+		if @previoushealth <= warrior.health
+			@ranged = false
+			puts 'Not Ranged!'
+		end
+		if @ranged
+			if warrior.feel.enemy?
+				warrior.attack!
+				@previousaction = :attack
+			else
+				warrior.walk!
+				@previousaction = :advance
+			end
+		else
+			if warrior.health < 10
+				if warrior.feel.enemy?
+					warrior.walk!(:backward)
+					@previousaction = :retreat
+				else
+					warrior.rest!
+					@previousaction = :rest
+				end
+			else
+				if warrior.feel.enemy?
+					warrior.attack!
+					@previousaction = :attack
+				else
+					warrior.walk!
+					@previousaction = :advance
+				end
+			end
 		end
 	end
+	@previoushealth = warrior.health
   end
 end
