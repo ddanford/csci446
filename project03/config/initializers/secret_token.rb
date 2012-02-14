f851555f8bb474237ee4ac8aef1d231c83e5c33a
David Danford <ddanford@gmail.com>
2012-02-14 14:19:48 -0700

    Moving the base folders up to correct previous incorrect file structure.

---
+++ project03/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '0b5b50b0608af4788303353b6e8d759f3be1e5589c26b765960e0e343f5bfef3c120a8b21116b07a0054ba68e6ecdacc2c6531728493a11d1144d48d878a555d'
