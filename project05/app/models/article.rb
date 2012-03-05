a82f7d4f472d92feee6a1e3bc4275d19f4e0613a
David Danford <ddanford@gmail.com>
2012-03-04 17:21:25 -0700

    Updated article and author models.

--- project05/app/models/article.rb
+++ project05/app/models/article.rb
@@ -1,3 +1,15 @@
 class Article < ActiveRecord::Base
+	belongs_to :author
+	
+	cattr_reader :per_page
+	@@per_page = 10
+	
 	validates :author, :title, :body, presence: true
+	
+	before_save :create_new_author
+	
+	private
+		def create_new_author
+			create_author(:name => new_author_name)
+		end
 end
--- project05/app/models/author.rb
+++ project05/app/models/author.rb
@@ -1,2 +1,14 @@
 class Author < ActiveRecord::Base
+	has_many :articles
+	validate :name_not_pat
+	
+	has_attached_file :photo,
+		:styles => { :thumb => "100x100#", :small => "150x150>" }
+		
+	private
+		def name_not_pat
+			if name.downcase.include? "pat"
+				errors.add(:name, 'Pat is not allowed anywhere in the string, in any case!')
+			end
+		end
 end
